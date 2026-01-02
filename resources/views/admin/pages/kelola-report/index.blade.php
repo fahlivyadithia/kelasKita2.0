404c7a988829783ff3485d6473838eb859985366
ShafnatFR <shafnatfuainiramadhan@gmail.com>
2026-01-02 11:06:22 +0700

    feat: Add admin page for managing user reports with search and filter capabilities.

--- resources/views/admin/pages/kelola-report/index.blade.php
+++ resources/views/admin/pages/kelola-report/index.blade.php
@@ -43,10 +43,7 @@
                         </select>
                     </div>
 
-                    <button type="submit"
-                        class="rounded-md bg-slate-100 px-3 py-2 text-sm font-semibold text-slate-900 shadow-sm ring-1 ring-inset ring-slate-300 hover:bg-slate-50">
-                        Cari
-                    </button>
+
                 </form>
             </div>
         </div>
